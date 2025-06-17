00473adce4ddb5c6d28c26a6793c1f566cd0ec50
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2025-06-17 00:30:52 -0600

    Change to test CI/CD

--- apps/challenge-test/src/app/auth/pages/LoginPage.tsx
+++ apps/challenge-test/src/app/auth/pages/LoginPage.tsx
@@ -32,7 +32,7 @@ export const LoginPage = () => {
   };
 
   return (
-    <AuthLayout title="Login">
+    <AuthLayout title="Login Test">
       <form
         onSubmit={onSubmit}
         className="animate__animated animate__fadeIn animate__faster"
