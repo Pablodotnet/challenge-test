bce2c47351c99b1cbe6b84ef1d3fa419bc1225fa
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2025-06-18 19:41:40 -0600

    Fix test run

--- apps/challenge-test/src/app/app.spec.tsx
+++ apps/challenge-test/src/app/app.spec.tsx
@@ -1,15 +1,32 @@
 import { render } from '@testing-library/react';
-
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import App from './app';
+import { store } from './store';
+
+jest.mock('firebase/auth', () => {
+  return {
+    GoogleAuthProvider: jest.fn().mockImplementation(() => ({})),
+    getAuth: jest.fn(() => ({
+      currentUser: null,
+      signOut: jest.fn(),
+    })),
+    onAuthStateChanged: jest.fn((auth, callback) => {
+      callback(null);
+      return () => {};
+    }),
+  };
+});
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    );
     expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome challenge-test/gi)).toBeTruthy();
   });
 });
