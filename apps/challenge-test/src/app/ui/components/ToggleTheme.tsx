5e505569e68c5ecfa2d832bf7929612f39ce6131
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2025-06-18 22:44:02 -0600

    Prettier run after hook check

--- apps/challenge-test/src/app/ui/components/ToggleTheme.tsx
+++ apps/challenge-test/src/app/ui/components/ToggleTheme.tsx
@@ -14,7 +14,11 @@ export const ToggleTheme = () => {
   };
 
   return (
-    <IconButton data-testid="toggle-theme-btn" aria-label="toggle theme" onClick={handleToggle}>
+    <IconButton
+      data-testid="toggle-theme-btn"
+      aria-label="toggle theme"
+      onClick={handleToggle}
+    >
       {isDarkMode ? (
         <DarkModeIcon></DarkModeIcon>
       ) : (
